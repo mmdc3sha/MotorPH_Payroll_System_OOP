16a3ef619e3200cfd1e4cd8f8d00ee355ab34607
CocoPink <trishaquismundo408@gmail.com>
2025-02-21 03:43:34 +0800

    MAIN

--- src/main/java/GUI/AbstractPayroll.java
+++ src/main/java/GUI/AbstractPayroll.java
@@ -1,7 +1,7 @@
 package GUI;
 
 public abstract class AbstractPayroll {
-    protected int payrollId;
+    protected String payslip_no;
     protected String payPeriodStart;
     protected String payPeriodEnd;
     protected String employeeName;
@@ -13,16 +13,46 @@ public abstract class AbstractPayroll {
     protected int days_worked;
     protected int overtime;
     protected double totalGrossIncome;
-
-
-    protected double totalNetIncome;
-    protected double totalDeductions;
+    protected double rice_subsidy;
+    protected double phone_allowance;
+    protected double clothing_allowance;
     protected double totalBenefits;
+
+    public AbstractPayroll(String payslip_no, String payPeriodStart, String payPeriodEnd,
+                           String employeeName, String firstName, String lastName, String emp_position,
+                           double monthly_rate, double daily_rate, int days_worked, int overtime, double totalGrossIncome,
+                           double rice_subsidy, double phone_allowance, double clothing_allowance, double totalBenefits, double totalSSSDeduction,
+                           double totalPhilHealthDeduction, double totalPagIbigDeduction, double totalWithholdingTax, double totalDeductions, double totalNetIncome) {
+        this.payslip_no = payslip_no;
+        this.payPeriodStart = payPeriodStart;
+        this.payPeriodEnd = payPeriodEnd;
+        this.employeeName = employeeName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emp_position = emp_position;
+        this.monthly_rate = monthly_rate;
+        this.daily_rate = daily_rate;
+        this.days_worked = days_worked;
+        this.overtime = overtime;
+        this.totalGrossIncome = totalGrossIncome;
+        this.rice_subsidy = rice_subsidy;
+        this.phone_allowance = phone_allowance;
+        this.clothing_allowance = clothing_allowance;
+        this.totalBenefits = totalBenefits;
+        this.totalSSSDeduction = totalSSSDeduction;
+        this.totalPhilHealthDeduction = totalPhilHealthDeduction;
+        this.totalPagIbigDeduction = totalPagIbigDeduction;
+        this.totalWithholdingTax = totalWithholdingTax;
+        this.totalDeductions = totalDeductions;
+        this.totalNetIncome = totalNetIncome;
+    }
+
     protected double totalSSSDeduction;
     protected double totalPhilHealthDeduction;
     protected double totalPagIbigDeduction;
     protected double totalWithholdingTax;
-    protected double totalThirteenthMonthPay;
+    protected double totalDeductions;
+    protected double totalNetIncome;
 
 
     // Abstract method to be implemented by subclasses
