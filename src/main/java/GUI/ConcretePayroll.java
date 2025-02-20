430cef85d32336199da7a369f43c003ddf0d1b2f
CocoPink <trishaquismundo408@gmail.com>
2025-02-21 03:48:52 +0800

    MAIN

--- src/main/java/GUI/ConcretePayroll.java
+++ src/main/java/GUI/ConcretePayroll.java
@@ -1,4 +1,16 @@
 package GUI;
 
-public class ConcretePayroll {
+public class ConcretePayroll extends Payroll implements PayrollOperationsInterface{
+    public ConcretePayroll() {
+
+    }
+
+    @Override
+    public void insertPayroll_to_Database() {
+        DatabaseConnection db = new DatabaseConnection();
+        db.connectToDatabase();
+
+        //String insertPayrollSQL = "";
+
+    }
 }
